8be174e426c981741837d33cc5700f5f364aa4df
guoshuaiping <guoshuaiping@zerozero.cn>
2021-12-29 14:53:32 +0800

    给服务消费方的RestTemplate添加@LoadBalanced注解后，使用服务提供者的名称进行远程调用

--- order-service/src/main/java/com/gsp/order/service/OrderService.java
+++ order-service/src/main/java/com/gsp/order/service/OrderService.java
@@ -10,10 +10,10 @@ public class OrderService {
     private RestTemplate restTemplate;
 
     public String queryUserNameByOrderId(Long orderId) {
-        //此时还无法使用服务名进行访问，回报错：java.net.UnknownHostException: user-service
-        //String url = "http://user-service/userName/"+orderId;
         // 可以使用如下直接访问服务提供方ip+端口方式直接访问，但有弊端，后续需要改进成通过服务名方式
-        String url = "http://localhost:8081/userName/"+orderId;
+        //String url = "http://localhost:8081/userName/"+orderId;
+        //给RestTemplate添加@LoadBalanced注解后，就可以使用服务提供者的名称进行远程调用了
+        String url = "http://user-service/userName/"+orderId;
         String userName = restTemplate.getForObject(url,String.class);
         return userName;
     }
