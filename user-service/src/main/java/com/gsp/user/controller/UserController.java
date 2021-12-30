bca181a02dd378a339ffc02b53d967b7bcebb79c
gsp <1131464222@qq.com>
2021-12-30 08:51:50 +0800

    通过在@Value注入配置值所在类上添加@RefreshScope的方式来实现nacos配置热更新

--- user-service/src/main/java/com/gsp/user/controller/UserController.java
+++ user-service/src/main/java/com/gsp/user/controller/UserController.java
@@ -3,6 +3,7 @@ package com.gsp.user.controller;
 import com.gsp.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,11 +14,14 @@ import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
 @RestController
+@RefreshScope
 public class UserController {
     @Autowired
     private UserService userService;
     @Value("${pattern.dateformat}")
     private String dateformat;
+    @Value("${hotUpdate.myName}")
+    private String myName;
 
     @GetMapping("/userName/{orderId}")
     public String getUserNameByOrderId(@PathVariable("orderId") Long orderId){
@@ -27,6 +31,7 @@ public class UserController {
     @GetMapping("now")
     public String now(){
         System.out.println(dateformat);
+        System.out.println("热更新：myName="+myName);
         return LocalDateTime.now().format(DateTimeFormatter.ofPattern(dateformat, Locale.CHINA));
     }
 }
