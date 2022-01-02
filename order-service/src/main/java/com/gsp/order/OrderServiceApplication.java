4445642afaf6006d57e22b79b4f3d504762dc6eb
gsp <1131464222@qq.com>
2022-01-02 17:42:12 +0800

    开启日志服务，并配置Feign的日志

--- order-service/src/main/java/com/gsp/order/OrderServiceApplication.java
+++ order-service/src/main/java/com/gsp/order/OrderServiceApplication.java
@@ -1,5 +1,6 @@
 package com.gsp.order;
 
+import com.gsp.order.config.FeignClientConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
@@ -7,7 +8,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@EnableFeignClients
+@EnableFeignClients(defaultConfiguration = FeignClientConfiguration.class)
 @SpringBootApplication
 public class OrderServiceApplication {
 
